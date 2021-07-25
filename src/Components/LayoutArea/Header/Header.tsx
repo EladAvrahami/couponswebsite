079610f216757a347cf1b2319c6ad45a40781a06
sidorovichva <sidorovichva@gmail.com>
2021-07-25 13:18:55 +0300

    Header.tsx conditional rendering based on clientType

--- src/Components/LayoutArea/Header/Header.tsx
+++ src/Components/LayoutArea/Header/Header.tsx
@@ -2,7 +2,7 @@ import { NavLink, useHistory } from "react-router-dom";
 import "./Header.css";
 import {useState} from "react";
 import store from "../../../Redux/Store";
-import Aside from "../Aside/Aside";
+import {UserInterface} from "../../../interfaces/UserInterface";
 
 function Header():JSX.Element{
     var history = useHistory();
@@ -12,34 +12,28 @@ function Header():JSX.Element{
 
     // const [clientType] = useState(store.getState().authState.user.clientType) //taken from Aside.tsx line 10
     // const [clientType] = useState(localStorage.getItem("user")) // maybe you can use just this instead?
-    const [clientType] = useState("admin") //example
+    const [user, setUser] = useState<UserInterface>(JSON.parse(localStorage.getItem("user"))); //example
 
     return(
-        <div className="Header ">   {/*header d-flex justify-content-center py-3 shadow-sm   **** my bootstrap*/}
-            {/* <div>logo</div> */}
-
-
-            <button className="loginButton btn btn-primary btn-s mx-2" onClick={Login}>Login</button>
-            
-            <Aside/>
-
-            {/* {clientType === 'admin' && <div>
+        <div className="Header">
+            <button className="loginButton" onClick={Login}>Login</button>
+            {user.clientType === 'admin' && <div>
                 <NavLink className="navLink" exact to="/GetCustomerCoupons/" >Get customer coupons</NavLink>
                 <NavLink className="navLink" exact to="/GetCompanyCoupons/" >Get company coupons</NavLink>
                 <NavLink className="navLink" exact to="/CustomersTable" >Manage customers</NavLink>
                 <NavLink className="navLink" exact to="/CompaniesTable" >Manage companies</NavLink>
             </div>}
-            {clientType === 'company' && <div>
+            {user.clientType === 'company' && <div>
                 <NavLink className="navLink" exact to="/GetCompanyCouponsByMaxPrice/" >Coupons by max-price</NavLink>
                 <NavLink className="navLink" exact to="/GetCompanyDetails">Company details</NavLink>
                 <NavLink className="navLink" exact to="/SingleCompanyTable">Coupons management</NavLink>
             </div>}
-            {clientType === 'customer' && <div>
+            {user.clientType === 'customer' && <div>
                 <NavLink className="navLink" exact to="/CouponPurchase" >Purchase coupons</NavLink>
                 <NavLink className="navLink" exact to="/MyPurchasedCouponsByMaxPrice/" >My coupons by max-price</NavLink>
                 <NavLink className="navLink" exact to="/GetCustomerDetails" >My details</NavLink>
                 <NavLink className="navLink" exact to="/SingleCustomerTable/" >My coupons</NavLink>
-            </div>} */}
+            </div>}
         </div>
     );
 }
